2deb37ed34a01e688ac3f289a9d196d7ea423dcb
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-01-05 18:47:28 +0300

    Initial Commit

---
+++ Itenerary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itenerary
+//
+//  Created by Abdulaziz AlObaili on 05/01/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
