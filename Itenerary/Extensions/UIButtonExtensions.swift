989e0ccaf277250eb1c075eef93951138506dc03
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-01-11 23:00:54 +0300

    Add a floating action button

---
+++ Itenerary/Extensions/UIButtonExtensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIButtonExtensions.swift
+//  Itenerary
+//
+//  Created by Abdulaziz AlObaili on 11/01/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func createFloatingActionButton() {
+        backgroundColor = Theme.tint
+        layer.cornerRadius = frame.height / 2
+        tintColor = .white
+        layer.shadowOpacity = 0.25
+        layer.shadowRadius = 5
+        layer.shadowOffset = CGSize(width: 0, height: 10)
+    }
+}
--- Itenerary/Views/TripsViewController/TripsViewController.swift
+++ Itenerary/Views/TripsViewController/TripsViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class TripsViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +24,8 @@ class TripsViewController: UIViewController {
         }
         
         view.backgroundColor = Theme.background
+        addButton.createFloatingActionButton()
+        
     }
     
 }
