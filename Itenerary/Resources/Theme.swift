32b1faf68bee2ad55d4481d75ec3192006f37c39
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-01-11 22:34:59 +0300

    Add a theme for the app

---
+++ Itenerary/Resources/Theme.swift
@@ -0,0 +1,18 @@
+//
+//  Theme.swift
+//  Itenerary
+//
+//  Created by Abdulaziz AlObaili on 11/01/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+class Theme {
+    
+    static let mainFontName = "FugazOne-Regular"
+    static let accent = UIColor(named: "Accent")
+    static let background = UIColor(named: "Background")
+    static let tint = UIColor(named: "Tint")
+    
+}
