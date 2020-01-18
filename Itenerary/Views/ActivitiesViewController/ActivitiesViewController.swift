bf50f53cd5e5d08217be95baf458551f33b35b20
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2020-01-18 23:50:15 +0300

    Add initial trip details screen

---
+++ Itenerary/Views/ActivitiesViewController/ActivitiesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ActivitiesViewController.swift
+//  Itenerary
+//
+//  Created by Abdulaziz AlObaili on 18/01/2020.
+//  Copyright © 2020 Abdulaziz AlObaili. All rights reserved.
+//
+
+import UIKit
+
+class ActivitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func back(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+}
